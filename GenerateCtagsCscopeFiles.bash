3cb00e39df8343540dc1bae48d48965df0f35e77
Allan Ing <xeero@yahoo.com>
2018-06-18 21:37:41 -0500

    Simplify Generate .bash

--- GenerateCtagsCscopeFiles.bash
+++ GenerateCtagsCscopeFiles.bash
@@ -5,9 +5,6 @@ CTAGS_EXE=/usr/bin/ctags
 CSCOPE_EXE=/usr/bin/cscope
 FIND_EXE=/usr/bin/find
 
-# Project directories
-PROJ_DIRS=( RP_SW MDS_TC )
-
 # Relative paths to source code
 SRC_TOP_DIRS=( RP_SW/SKR_Software_Source/Model
                RP_SW/SKR_Software_Source/External_Source
@@ -17,31 +14,26 @@ SRC_TOP_DIRS=( RP_SW/SKR_Software_Source/Model
 START_DIR=`pwd`
 echo Base directory: $START_DIR
 
-# Process each project directory, e.g. RP_SW~ai01
-for PROJ_DIR in ${PROJ_DIRS[@]}
+# Process each directory, e.g. RP_SW~ai01
+# Each subdirectory at this level should be a Synergy project
+for PROJ_DIR in *
 do
-  MY_PROJ_DIRS=${PROJ_DIR}~*
-  for MY_PROJ_DIR in $MY_PROJ_DIRS
+  # Iterate directories within the project
+  for SRC_TOP_DIR in ${SRC_TOP_DIRS[@]}
   do
-    if [ -d "$MY_PROJ_DIR" ]; then
-      # Iterate directories within the project
-      for SRC_TOP_DIR in ${SRC_TOP_DIRS[@]}
-      do
-        SRC_DIR=$MY_PROJ_DIR/$SRC_TOP_DIR
-        if [ -d $SRC_DIR ]; then
-          echo Processing $SRC_DIR
-          cd $SRC_DIR
+    SRC_DIR=$PROJ_DIR/$SRC_TOP_DIR
+    if [ -d $SRC_DIR ]; then
+      echo Processing $SRC_DIR
+      cd $SRC_DIR
 
-          echo ...ctags
-          $CTAGS_EXE --recurse
+      echo ...ctags
+      $CTAGS_EXE --recurse
 
-          echo ...cscope
-          $FIND_EXE . -name '*.c' -o -name '*.cpp' -o -name '*.h' > cscope.files
-          $CSCOPE_EXE -b -i cscope.files -f cscope.out
+      echo ...cscope
+      $FIND_EXE . -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.java' > cscope.files
+      $CSCOPE_EXE -b -i cscope.files -f cscope.out
 
-          cd $START_DIR
-        fi
-      done
+      cd $START_DIR
     fi
   done
 done
